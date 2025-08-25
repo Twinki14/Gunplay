4b5cb838a0a84d43b9f8cd4d9f9b6c93b1eec258
Twinki <Twinki14@users.noreply.github.com>
2025-08-24 23:31:41 -0400

    exactPosition NRE

--- Source/ProjectileTrail.cs
+++ Source/ProjectileTrail.cs
@@ -1,136 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
 using Verse;
 
 namespace Gunplay
 {
-    class ProjectileTrail : ThingWithComps
+    internal class ProjectileTrail : ThingWithComps
     {
-        Matrix4x4 drawingMatrix = default;
+        private Matrix4x4 _drawingMatrix;
 
-        int ticksUtilDeath;
-        int ticksIdle;
-        Projectile projectile;
-        Vector3 a;
-        Vector3 exactPosition;
-        Vector3 previousPosition;
-        float speed;
-        Vector3 dir;
-        float length;
-        float width;
+        private int _ticksUtilDeath;
+        private int _ticksIdle;
+        private Projectile _projectile;
+        private Vector3 _a;
+        private Vector3 _exactPosition;
+        private Vector3 _previousPosition;
+        private float _speed;
+        private Vector3 _dir;
+        private float _length;
+        private float _width;
 
         public void Initialize(Projectile proj, Vector3 destination, Thing equipment)
         {
-            ticksUtilDeath = -1;
-            ticksIdle = 0;
-            projectile = proj;
-            a = projectile.ExactPosition;
-            Vector3 b = destination;
-            a.y = b.y = proj.def.Altitude;
-            speed = proj.def.projectile.SpeedTilesPerTick;
-            exactPosition = a;
+            _ticksUtilDeath = -1;
+            _ticksIdle = 0;
+            _projectile = proj;
+            _a = _projectile.ExactPosition;
+            var b = destination;
+            _a.y = b.y = proj.def.Altitude;
+            _speed = proj.def.projectile.SpeedTilesPerTick;
+            _exactPosition = _a;
 
-            length = speed * 15f;
-            dir = (b - a).normalized;
-            width = proj.DamageAmount * 0.006f;
+            _length = _speed * 15f;
+            _dir = (b - _a).normalized;
+            _width = proj.DamageAmount * 0.006f;
 
-
-            ProjectileTrailDef trailDef = def as ProjectileTrailDef;
-            if(trailDef != null && trailDef.trailWidth != -1) width = trailDef.trailWidth;
+            if (def is ProjectileTrailDef trailDef && !Mathf.Approximately(trailDef.trailWidth, -1))
+            {
+                _width = trailDef.trailWidth;
+            }
         }
-
 
         public override void ExposeData()
         {
             base.ExposeData();
 
-            Scribe_References.Look(ref projectile, "projectile");
-            Scribe_Values.Look(ref a, "a");
-            Scribe_Values.Look(ref speed, "speed");
-            Scribe_Values.Look(ref dir, "dir");
-            Scribe_Values.Look(ref width, "width");
-            Scribe_Values.Look(ref exactPosition, "exactPosition");
-            Scribe_Values.Look(ref ticksUtilDeath, "ticksUtilDeath");
+            Scribe_References.Look(ref _projectile, "projectile");
+            Scribe_Values.Look(ref _a, "a");
+            Scribe_Values.Look(ref _speed, "speed");
+            Scribe_Values.Look(ref _dir, "dir");
+            Scribe_Values.Look(ref _width, "width");
+            Scribe_Values.Look(ref _exactPosition, "exactPosition");
+            Scribe_Values.Look(ref _ticksUtilDeath, "ticksUtilDeath");
         }
 
-        public override Vector3 DrawPos
-        {
-            get
-            {
-                return exactPosition;
-            }
-        }
+        public override Vector3 DrawPos => _exactPosition;
 
-        float TicksPerLength => length / speed;
+        private float TicksPerLength => _length / _speed;
 
         protected override void Tick()
         {
             base.Tick();
 
-            if (projectile != null)
+            if (_projectile != null)
             {
-                exactPosition = projectile.ExactPosition;
-                exactPosition.y = projectile.def.Altitude;
-
-                if (projectile.Destroyed || (ticksUtilDeath == -1 && ticksIdle>10 && previousPosition == exactPosition))
+                if (_projectile.Destroyed || _projectile?.ExactPosition is null)
                 {
-                    projectile = null;
-                    ticksUtilDeath = Mathf.CeilToInt(TicksPerLength);
+                    _projectile = null;
+                    _ticksUtilDeath = Mathf.CeilToInt(TicksPerLength);
+                }
+                else
+                {
+                    _exactPosition = _projectile.ExactPosition;
+                    _exactPosition.y = _projectile.def.Altitude;
+
+                    if (_ticksUtilDeath == -1 && _ticksIdle > 10 && _previousPosition == _exactPosition)
+                    {
+                        _projectile = null;
+                        _ticksUtilDeath = Mathf.CeilToInt(TicksPerLength);
+                    }
                 }
             }
 
-            if (previousPosition == exactPosition)
-                ticksIdle++;
+            if (_previousPosition == _exactPosition)
+                _ticksIdle++;
 
-            previousPosition = exactPosition;
+            _previousPosition = _exactPosition;
 
-            Position = exactPosition.ToIntVec3();
+            Position = _exactPosition.ToIntVec3();
 
-            if (ticksUtilDeath > 0)
-                ticksUtilDeath--;
-            else if(ticksUtilDeath == 0)
+            if (_ticksUtilDeath > 0)
+                _ticksUtilDeath--;
+            else if(_ticksUtilDeath == 0)
                 Destroy(DestroyMode.Vanish);
         }
 
         protected override void DrawAt(Vector3 drawLoc, bool flip = false)
         {
-            float traveled = (a - exactPosition).magnitude;
+            var traveled = (_a - _exactPosition).magnitude;
             float len;
             Vector3 drawPos;
-            if (traveled < length)
+            if (traveled < _length)
             {
                 len = traveled;
-                if (ticksUtilDeath == -1)
+                if (_ticksUtilDeath == -1)
                 {
-                    drawPos = (a + exactPosition) / 2;
+                    drawPos = (_a + _exactPosition) / 2;
                 }
                 else
                 {
-                    len *= ticksUtilDeath / TicksPerLength;
-                    drawPos = exactPosition - dir * len / 2;
+                    len *= _ticksUtilDeath / TicksPerLength;
+                    drawPos = _exactPosition - _dir * len / 2;
                 }
             }
-            else if (ticksUtilDeath != -1)
+            else if (_ticksUtilDeath != -1)
             {
-                len = length * ticksUtilDeath / TicksPerLength;
-                drawPos = exactPosition - dir * len / 2;
+                len = _length * _ticksUtilDeath / TicksPerLength;
+                drawPos = _exactPosition - _dir * len / 2;
             }
             else
             {
-                len = length;
-                drawPos = exactPosition - dir * len / 2;
+                len = _length;
+                drawPos = _exactPosition - _dir * len / 2;
             }
 
-            Vector3 drawingScale = new Vector3(width, 1f, len);
-            drawingMatrix.SetTRS(drawPos, Quaternion.LookRotation(dir), drawingScale);
-            Graphics.DrawMesh(MeshPool.plane10, drawingMatrix, Graphic.MatSingle, 0);
+            var drawingScale = new Vector3(_width, 1f, len);
+            _drawingMatrix.SetTRS(drawPos, Quaternion.LookRotation(_dir), drawingScale);
+            Graphics.DrawMesh(MeshPool.plane10, _drawingMatrix, Graphic.MatSingle, 0);
 
             Comps_PostDraw();
         }
-
     }
 }
