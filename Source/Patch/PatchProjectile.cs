7f9f8609f88053e62fc4734233089eabac141a95
Twinki <Twinki14@users.noreply.github.com>
2025-08-25 01:46:39 -0400

    Fix grenade animation

--- Source/Patch/PatchProjectile.cs
+++ Source/Patch/PatchProjectile.cs
@@ -1,10 +1,5 @@
 ﻿using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 using Verse;
 using Verse.Sound;
@@ -12,67 +7,82 @@ using Verse.Sound;
 namespace Gunplay.Patch
 {
 
-
-    [HarmonyPatch(typeof(Projectile), "Launch", new Type[] { typeof(Thing), typeof(Vector3), typeof(LocalTargetInfo), typeof(LocalTargetInfo), typeof(ProjectileHitFlags), typeof(bool), typeof(Thing), typeof(ThingDef) })]
-    class PatchProjectileLaunch
+    [HarmonyPatch(typeof(Projectile), "Launch", typeof(Thing), typeof(Vector3), typeof(LocalTargetInfo), typeof(LocalTargetInfo), typeof(ProjectileHitFlags), typeof(bool), typeof(Thing), typeof(ThingDef))]
+    public class PatchProjectileLaunch
     {
         static PropertyInfo StartingTicksToImpactProp = typeof(Projectile).GetProperty("StartingTicksToImpact", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        static void Postfix(Projectile __instance, Vector3 ___destination, Thing launcher, ref Vector3 ___origin, LocalTargetInfo intendedTarget, Thing equipment, ref int ___ticksToImpact)
+        public static void Postfix(Projectile __instance, Vector3 ___destination, Thing launcher, ref Vector3 ___origin, LocalTargetInfo intendedTarget, Thing equipment, ref int ___ticksToImpact)
         {
-            GunPropDef prop = GunplaySetup.GunProp(equipment);
-            if (prop == null) return;
+            var prop = GunplaySetup.GunProp(equipment);
+            if (prop is null)
+            {
+                return;
+            }
 
-            CompGun comp = equipment.TryGetComp<CompGun>();
+            var comp = equipment.TryGetComp<CompGun>();
             if (comp != null)
             {
-                float angle = (___destination - ___origin).AngleFlat() - (intendedTarget.CenterVector3 - ___origin).AngleFlat();
+                var angle = (___destination - ___origin).AngleFlat() - (intendedTarget.CenterVector3 - ___origin).AngleFlat();
                 if (angle < -180) {
                     angle += 360;
                 }
                 comp.RotationOffset = (angle + 180) % 360 - 180;
             }
 
-            if (launcher as Pawn != null)
+            if (launcher is Pawn)
             {
-                ___origin += (___destination - ___origin).normalized * prop.barrelLength;
+                // This is mainly meant for grenades, which we don't want to affect the origin point of.
+                // When grenades have their origin point moved, it affects the "landing" animation,
+                // and "resets" the grenades animation when it reaches the target tile.
+                if (__instance.def.projectile.explosionDelay <= 0)
+                {
+                    ___origin += (___destination - ___origin).normalized * prop.barrelLength;
+                }
+
                 ___ticksToImpact = Mathf.CeilToInt((float) StartingTicksToImpactProp.GetValue(__instance));
-                if (___ticksToImpact < 1) ___ticksToImpact = 1;
+                if (___ticksToImpact < 1)
+                {
+                    ___ticksToImpact = 1;
+                }
             }
 
-            if (Gunplay.settings.enableTrails && launcher?.Map!=null)
+            if (!Gunplay.settings.enableTrails || launcher?.Map == null)
             {
-                ProjectileTrail trail = GenSpawn.Spawn(prop.trail, ___origin.ToIntVec3(), launcher.Map, WipeMode.Vanish) as ProjectileTrail;
-                trail.Initialize(__instance, ___destination, equipment);
+                return;
+            }
+
+            if (GenSpawn.Spawn(prop.trail, ___origin.ToIntVec3(), launcher.Map) is ProjectileTrail trail)
+            {
+                trail.Initialize(__instance, ___destination);
             }
         }
     }
 
     [HarmonyPatch(typeof(Projectile), "get_StartingTicksToImpact")]
-    class PatchProjectileStartingTicksToImpact
+    public class PatchProjectileStartingTicksToImpact
     {
-        static float Postfix(float value, Projectile __instance)
+        public static float Postfix(float value, Projectile __instance)
         {
-            GunPropDef prop = GunplaySetup.GunProp(__instance.EquipmentDef);
+            var prop = GunplaySetup.GunProp(__instance.EquipmentDef);
             if (prop == null || prop.preserveSpeed) return value;
 
             return value / Gunplay.settings.projectileSpeed;
         }
-
     }
 
     [HarmonyPatch(typeof(Projectile), "Impact")]
-    class PatchProjectileImpact
+    public class PatchProjectileImpact
     {
-        static void Prefix(Projectile __instance, Thing hitThing, Vector3 ___origin)
+        public static void Prefix(Projectile __instance, Thing hitThing, Vector3 ___origin)
         {
-            Map map = __instance.Map;
+            var map = __instance.Map;
             if (map == null) return;
 
-            GunPropDef prop = GunplaySetup.GunProp(__instance.EquipmentDef);
+            var prop = GunplaySetup.GunProp(__instance.EquipmentDef);
             if (prop == null) return;
 
-            MaterialKind kind = MaterialKind.None;
+            var kind = MaterialKind.None;
 
             if (hitThing != null)
             {
@@ -81,21 +91,21 @@ namespace Gunplay.Patch
 
             if(kind == MaterialKind.None)
             {
-                TerrainDef terrainDef = map.terrainGrid.TerrainAt(CellIndicesUtility.CellToIndex(__instance.Position, map.Size.x));
+                var terrainDef = map.terrainGrid.TerrainAt(CellIndicesUtility.CellToIndex(__instance.Position, map.Size.x));
                 kind = MaterialKindGetter.Get(terrainDef);
             }
 
             if (Gunplay.settings.enableSounds) {
-                SoundDef sound = prop.projectileImpactSound == null ? null : prop.projectileImpactSound.Effect(kind);
+                var sound = prop.projectileImpactSound == null ? null : prop.projectileImpactSound.Effect(kind);
                 if (sound != null) sound.PlayOneShot(new TargetInfo(__instance.Position, map, false));
             }
 
             if (Gunplay.settings.enableEffects)
             {
-                EffecterDef effecterDef = prop.projectileImpactEffect == null ? null : prop.projectileImpactEffect.Effect(kind);
+                var effecterDef = prop.projectileImpactEffect?.Effect(kind);
                 if (effecterDef != null)
                 {
-                    Effecter effecter = new Effecter(effecterDef);
+                    var effecter = new Effecter(effecterDef);
                     effecter.Trigger(__instance, new TargetInfo(___origin.ToIntVec3(), __instance.Map));
                     effecter.Cleanup();
                 }
